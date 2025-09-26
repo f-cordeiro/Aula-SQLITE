61f52f887e670362b9c4c93c0ae69a40d99d5c46
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2025-09-26 11:15:52 -0300

    adicionar os valores na tabela alunos

--- main.py
+++ main.py
@@ -7,12 +7,36 @@ conexao = sqlite3.connect("escola.db")
 # Criar o objeto chamado de "cursor" que será usado para executar os comandos sql
 cursor = conexao.cursor()
 
-# Criar uma tabela no banco
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS alunos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    idade INTEGER,
-    curso TEXT
-    )
-""")
+# # Criar uma tabela no banco
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS alunos (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     nome TEXT NOT NULL,
+#     idade INTEGER,
+#     curso TEXT
+#     )
+# """)
+# nome = input("Digite o nome do aluno(a): ").lower()
+# idade = int(input(f"Digite a idade do aluno {nome}: "))
+# curso = input(f"Digite o curso do aluno {nome}: ").lower()
+
+
+
+# Inserir vários alunos de uma vez 
+alunos = [
+    ("Yago", 28, "Direito"),
+    ("Jessica", 24, "Computação"),
+    ("Breno", 28, "Computaria"),
+]
+
+# Inserir um dado na tabela - executemany serve para add vairos
+cursor.executemany("""
+INSERT INTO alunos (nome, idade, curso)
+VALUES (?, ?, ?)
+""",
+
+(alunos)
+)
+
+#Confirmar as alterações no banco
+conexao.commit()
